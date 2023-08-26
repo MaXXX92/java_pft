a4af2c0b77b71ec6482b15b28025d7adc7dd1662
Max <grigoriev92@yahoo.com>
2023-08-26 23:39:28 +0300

    Reformat code

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstClass.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstClass.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstClass {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 }
